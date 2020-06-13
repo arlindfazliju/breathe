eba0b99d3f3c03cc2bf62250ac119f3a577d0d72
arlindfazliju <arlindfazliu96@gmail.com>
2020-06-14 00:02:27 +0200

    changes

--- dist/script.js
+++ dist/script.js
@@ -1,21 +1,21 @@
 const container = document.getElementById("container");
 const text = document.getElementById("text");
 
-const totalTime = 7500;
+const totalTime = 6000;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
 breathAnimation();
 
 function breathAnimation() {
-  text.innerText = "Breathe In!";
+  text.innerText = "Merr Fryme!";
   container.className = "container grow";
 
   setTimeout(() => {
-    text.innerText = "Hold";
+    text.innerText = "Mbaje";
 
     setTimeout(() => {
-      text.innerText = "Breathe Out!";
+      text.innerText = "Leshoje frymen!";
       container.className = "container shrink";
     }, holdTime);
   }, breatheTime);
